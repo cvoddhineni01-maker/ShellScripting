30aaaf9d411bb2e64b174132e02d9a08e79b27f4
Chandrahasa <cvoddhineni01@gmail.com>
2026-03-11 22:27:23 -0500

    Changes

---
+++ 03_variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "This is Shell Scripting Github About Arguements"
+
+#
+
+PERSON1=$1
+PERSON2=$2
+
+echo "Hi $PERSON1"
+echo "Hi $PERSON2"
+
+#Command Subistution 
+
+echo "Please enter $(date)" 
+
+
+
---
+++ 04_variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This is Shell Scripting"
+
+echo "Please enter your PIN Number"
+#-s is used to hide
+read -s PIN_NUMBER 
+
+
+
+echo "Pin entered is $PIN_NUMBER"
+
+
