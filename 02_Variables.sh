4f103257de30a2973645a50ee43b98f719b787f7
Chandrahasa <cvoddhineni01@gmail.com>
2026-03-11 21:54:13 -0500

    Changes

--- 02_Variables.sh
+++ 02_Variables.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "This is Chandu Github About Varibles"
+echo "This is Chandu Github About Variables"
 
 PERSON1=SOUJU
 PERSON2=Vihaan
 
 echo "Hi $PERSON1"
 echo "Hi $PERSON2"
+
