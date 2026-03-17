3f27ff99cfc61c3fdff32a79581d36e404182c03
Chandrahasa <cvoddhineni01@gmail.com>
2026-03-17 14:20:00 -0500

    Changes

--- 09_install.sh
+++ 09_install.sh
@@ -21,12 +21,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
    echo "Error:: Please run the script with root privelage"
+   exit 1
 fi 
 
 dnf install mysql -y 
 
 if [ $? -ne 0 ]; then 
     echo "Error:: Installing MySQL Failure "
+    exit 1
 else
     echo "Installing MySql is Success"
 fi
